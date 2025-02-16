90eeeaafee9208708ecdebcf0e1dd6df1af1d97e
Nehaasati <31asatineha@gmail.com>
2025-02-16 11:48:45 +0100

    create product class in model

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import controller.CustomerController;
+//import controller.ProductController;
+
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws SQLException {
+
+        // JDBC URL för SQLite
+        String url = "jdbc:sqlite:C:/Users/nehaa/IdeaProjects/ecomerce_webshop1/webbutiken.db";
+
+        /*try (Connection connection = SqliteConnection.getConnection();
+             Statement stmt = connection.createStatement(URL);
+             ResultSet rs = stmt.executeQuery("SELECT * FROM customers")) {
+
+             //Loopa genom alla rader i resultatet
+            while (rs.next()) {
+                // Ändra kolumnnamnen nedan till de som finns i din tabell
+                System.out.println(
+                       rs.getInt("customer_id") + " | " +
+                             rs.getString("first_name")+"|"+
+                               rs.getString("email");
+
+                       // Lägg till fler kolumner efter behov);
+                        }
+
+        } catch (SQLException e) {
+           System.out.println("Ett fel uppstod: " + e.getMessage());
+        }*/
+        //CustomerService service = new CustomerService();
+        //service.showAllCustomers();
+
+        CustomerController customerController = new CustomerController();
+        customerController.run();
+        //ProductController productController = new ProductController();
+        //productController.run();
     }
+
+
+//customers cs =new customers(9,"neha","31@ngmail.com","232312");
+//CustomerRepository cr = new CustomerRepository();
+//cr.getCustomerById(2);
 }
+
+
+
